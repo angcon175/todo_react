d15c5e8cd646a0f2ce199f070467b0ea53619de6
angcon175 <angcon26011998@gmail.com>
2024-09-26 04:12:43 +0600

    todo

---
+++ src/Todo.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import "./Todo.css";
+
+export default function Todo(){
+    let [todos,setTodos] = useState([{task: "sample-task", id: uuidv4()}])
+    let [newTodo, setNewTodo] = useState("")
+
+    let addNewTask = () => {
+        setTodos((prevTodos) => {
+            return [...prevTodos, {task: newTodo, id: uuidv4()}]
+        })
+        setNewTodo("")
+    }
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value)
+    }
+
+    let deleteTodo = (id) => {
+       setTodos (todos.filter((todo) => todo.id != id))
+    }
+
+
+    return (
+        <>
+            <input className="inp" value={newTodo}
+            onChange={updateTodoValue}
+            placeholder="add a task"></input> 
+            &nbsp; &nbsp;
+            <button className="btnn" onClick={addNewTask}>Add Task</button>
+            
+            <hr style={{ border: '1px solid black' }} />
+            <h4>Todo</h4>
+            <ul>
+                {todos.map((todo) => (
+                <li key = {todo.id}>
+                <span>{todo.task}</span>
+                &nbsp; &nbsp;
+                <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
+                </li>
+                ))}
+            </ul>
+        </>
+    )
+}   
